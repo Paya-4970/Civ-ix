677031781eb8015126e7b4eb023e9c8d4e87e018
paya <payahatami7777@gmail.com>
2025-07-02 01:42:26 -0700

    First

---
+++ MyUser/models.py
@@ -0,0 +1,52 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+
+
+class MyUserManager(BaseUserManager):
+    def create_user(self, username, phone, password=None):
+        if not phone:
+            raise ValueError("Users must have a phone number")
+
+        user = self.model(
+            username=username,
+            phone=phone,
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, phone, password=None):
+        user = self.create_user(
+            username=username,
+            phone=phone,
+            password=password,
+        )
+        user.is_superuser = True
+        user.is_admin = True
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
+
+
+class MyUser(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(max_length=200, unique=True)
+    phone = models.CharField(max_length=200, unique=True)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    created_time = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField(auto_now=True)
+
+    objects = MyUserManager()
+
+    USERNAME_FIELD = 'phone'
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return self.username
+
+    def has_perm(self, perm, obj=None):
+        return self.is_superuser or self.is_admin
+
+    def has_module_perms(self, app_label):
+        return self.is_superuser or self.is_admin
