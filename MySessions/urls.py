813da47756bf80d690c521646524ef4be04cd502
paya <payahatami7777@gmail.com>
2025-07-06 00:59:37 -0700

    add test(scenario)

--- MySessions/urls.py
+++ MySessions/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('add/vote/<int:session>/', views.add_vote, name='vote-form'),
     path('session_list', views.mysession_list, name='session_list'),
     path('add/session', views.add_mysession, name='session-form'),
+    path('add/participant', views.add_participant, name='participant-form'),
 ]
 
--- scenario/tests.py
+++ scenario/tests.py
@@ -1,3 +1,44 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from .models import Scenario, Role
+from MyUser.models import MyUser
+from django.urls import reverse
 
-# Create your tests here.
+class ScenarioMakeTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = MyUser.objects.create_user(
+            username = 'testmain',
+            phone = '09121212',
+            password = '123456789987654321'
+        )
+        self.user.is_main = True
+        self.user.save()
+        self.client.login(username='09121212', password='123456789987654321')
+        
+        self.scenario = Scenario.objects.create(
+            title='test',
+            description = 'test',
+        )
+        # print(self.user)
+    
+    def test_if_scenario_made(self):
+        scenario = Scenario.objects.filter(title = 'test').exists()
+        self.assertTrue(scenario)
+        
+    def test_user_if_is_main(self):
+        user = MyUser.objects.filter(phone='09121212').exists()
+        self.assertTrue(user)
+        
+    def test_make_role(self):
+        data = {
+            'scenario': self.scenario.id,
+            'name' : 'doctor',
+            'description' : 'this is doctor'
+        }
+        self.client.post(reverse('scenario:role_form'), data)
+        self.assertEqual(Role.objects.filter(name='doctor').exists(),True)
+        
+    def test_login_success(self):
+        logged_in = self.client.login(username='09121212', password='123456789987654321')
+        self.assertTrue(logged_in)
+            
